f04414a6e5b6ea7d7b32f503a6770e30d1644cc4
Yu Lu <2577021958@qq.com>
2023-10-23 09:35:40 +0800

    update index page

--- docs/build/html/_static/documentation_options.js
+++ docs/build/html/_static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '0.0.1a',
-    LANGUAGE: 'zh_CN',
+    LANGUAGE: '',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
